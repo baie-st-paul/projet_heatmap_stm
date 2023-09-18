dd0f95675a3b139085cbdf9b0635c05cdbbebfba
Philippe Vallerand <pvallerand1@gmail.com>
2023-09-18 15:29:19 -0400

    ajout de annotation dataclass pour la classe coordinate

--- flaskProject/models/coordinate.py
+++ flaskProject/models/coordinate.py
@@ -1,14 +1,15 @@
 import math
+from dataclasses import dataclass
 from geopy import distance
 
+
+@dataclass
 class Coordinate:
-    def __init__(self, latitude, longitude, intensity_factor):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.intensity_factor = intensity_factor
+
+    latitude:float
+    longitude:float
+    intensity_factor:int
 
     def distance(self, other):
         return distance.distance((self.latitude, self.longitude), (other.latitude, other.longitude)).m
 
-    def __str__(self):
-        return "Latitude: " + str(self.latitude) + ", Longitude: " + str(self.longitude) + ", Intensity Factor: " + str(self.intensity_factor)
